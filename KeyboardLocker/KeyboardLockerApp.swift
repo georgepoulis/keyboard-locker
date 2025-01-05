fd9ac32305e4c5795e7f5e572e7cab18db380994
George Poulis <g.poulis@withplum.com>
2025-01-05 19:03:33 +0200

    Initial commit - Keyboard Locker App

---
+++ KeyboardLocker/KeyboardLockerApp.swift
@@ -0,0 +1,63 @@
+//
+//  KeyboardLockerApp.swift
+//  KeyboardLocker
+//
+//  Created by George Poulis on 5/1/25.
+//
+//
+import SwiftUI
+import Carbon
+
+@main
+struct KeyboardLockerApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        MenuBarExtra("KeyboardLocker", systemImage: "lock.fill") {
+            Button("Lock Keyboard") {
+                appDelegate.lockKeyboard()
+            }
+            Button("Unlock Keyboard") {
+                appDelegate.unlockKeyboard()
+            }
+            Divider()
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var eventTap: CFMachPort?
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // No need for explicit setup here as SwiftUI handles the MenuBarExtra
+    }
+
+    func lockKeyboard() {
+        if eventTap == nil {
+            let eventMask = CGEventMask(1 << CGEventType.keyDown.rawValue | 1 << CGEventType.flagsChanged.rawValue | 1 << CGEventType.keyUp.rawValue)
+            eventTap = CGEvent.tapCreate(tap: .cghidEventTap,
+                                         place: .headInsertEventTap,
+                                         options: .defaultTap,
+                                         eventsOfInterest: eventMask,
+                                         callback: { _, _, _,_  in return nil },
+                                         userInfo: nil)
+            if let eventTap = eventTap {
+                let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)!
+                CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+                CGEvent.tapEnable(tap: eventTap, enable: true)
+            }
+        }
+    }
+
+    func unlockKeyboard() {
+        if let eventTap = eventTap {
+            CGEvent.tapEnable(tap: eventTap, enable: false)
+            CFMachPortInvalidate(eventTap)
+            self.eventTap = nil
+        }
+    }
+}
+
